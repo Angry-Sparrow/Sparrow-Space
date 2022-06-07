7854b36d80c9596e0590ec5e85709597d67739ac
lirui141 <lirui141@ke.com>
2022-06-07 23:24:26 +0800

    chore: change siteUrl

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Sparrow-Space`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://sparrowspace.gatsbyjs.io/`
+    // siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin

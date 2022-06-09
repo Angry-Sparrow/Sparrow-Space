c03de288bbb0a09e42d84c0a81eef780f77fa196
Angry-Sparrow <beqan2016@163.com>
2022-06-09 10:57:07 +0800

    fix: path

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,7 +4,7 @@ import path from 'path'
 export const createPages: GatsbyNode['createPages'] = async ({ actions, graphql }) => {
     const { createPage } = actions
 
-    const docPostTemplate = path.resolve(`./src/templates/docTemplate.tsx`)
+    const docPostTemplate = path.resolve(`src/templates/docTemplate.tsx`)
 
     return graphql(`
     {
